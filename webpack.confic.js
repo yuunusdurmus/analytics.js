925559a962fccc1245eead7cb3e76e565fdeab20
Yunus Durmus <yunus.durmus@akinon.com>
2017-06-21 15:03:57 +0300

    fixed indentation

--- webpack.confic.js
+++ webpack.confic.js
@@ -1,14 +1,14 @@
 module.exports = {
-  devtool: "#inline-source-map",
-  entry: [
-    './index.js'
-  ],
+    devtool: "#inline-source-map",
+    entry: [
+        './index.js'
+    ],
 
-  output: {
-    filename: 'analytics.js',
-    publicPath: './',
-    library: 'analytics',
-    libraryTarget: 'umd'
-  },
+    output: {
+        filename: 'analytics.js',
+        publicPath: './',
+        library: 'analytics',
+        libraryTarget: 'umd'
+    },
 };
 
