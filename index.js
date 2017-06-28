f09aab70b74df06927e5a03a474386a598f49096
Yunus Durmus <yunus.durmus@akinon.com>
2017-06-28 20:05:02 +0300

    fixed

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-var Pages = require('./page-specific/pages');
+var analyticsActions = require('./lib/analyticsActions');
 
-function init() {
-    Pages.list.forEach(function(page){
-        if(page.selector == 'any'){
-            page.tracker();
-        }else if(page.selector == window.GLOBALS.page){
-            page.tracker();
-        }
-    })
+function arrayToPushAnalytics(arr) {
+    var arrayLength = arr.length;
+    var currentItem = null;
+
+    while(arrayLength > 0) {
+
+        currentItem = arr.pop();
+        
+        analyticsActions.actions.forEach(function(action){
+              if(currentItem.type == action.type){
+                  action.push(currentItem.data); 
+              }
+        })
+
+        arrayLength --;
+    }
+
 }
 
-init();
+window.AnalyticsArray = function(arr) {
+    var array = arr || [];
+    array.push = function(){
+        arrayToPushAnalytics([arguments[0]])
+        //return Array.prototype.push.apply(this, arguments);
+    }
+    
+    return array;
+};
+
+
+window.initAnalytics = function(arr) {
+   arrayToPushAnalytics(arr)
+}
