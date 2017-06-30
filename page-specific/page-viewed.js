5e75f94e3a274bf76605cde9473f3dd07c625255
Yunus Durmus <yunus.durmus@akinon.com>
2017-06-30 11:27:12 +0300

    removed needless file

--- page-specific/page-viewed.js
+++
@@ -1,8 +0,0 @@
-var analytics = require('../lib/analytics')
-
-module.exports = {
-    initTracker: function (){
-        var viewedMessage = window.ANALYTICS.viewedMessage || 'anonymous page viewed';
-        analytics.page(viewedMessage);
-    }
-}
