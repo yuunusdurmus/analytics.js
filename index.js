989daad7ac3666b4bf96241db4a06f3b3e4dfeaa
Yunus Durmus <yunus.durmus@akinon.com>
2017-07-03 11:47:17 +0300

    new analytics

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-var analytics = require('@segment/analytics.js-core');
 
-var configurations = [
-    {
-        package: require('@segment/analytics.js-integration-google-analytics'),
-        name: 'Google Analytics',
-        settings: {
-            trackingId: 'UA-XXXXXXXXX-X' 
+var Analytics = require('./library/analytics');
+var AnalyticsActions = require('./library/analyticsActions');
+
+module.exports = {
+    init: function(conf) {
+
+        var analytics = Analytics.create(conf);
+        var analyticsActions = AnalyticsActions.createActions(analytics)
+
+        function arrayToPushAnalytics(arr) {
+            var arrayLength = arr.length;
+            var currentItem = null;
+
+            while(arrayLength > 0) {
+
+                currentItem = arr.pop();
+        
+                analyticsActions.forEach(function(action){
+                    if(currentItem.type == action.type){
+                        action.push(currentItem.data); 
+                    }
+                })
+
+                arrayLength --;
+            }
+        }
+
+        window.AnalyticsArray = function(arr) {
+            var array = arr || [];
+            array.push = function(){
+                Array.prototype.push.apply(this, arguments);
+                arrayToPushAnalytics(this);
+                
+                return true;
+            }
+    
+            return array;
+        };
+
+        window.initAnalytics = function(arr) {
+            arrayToPushAnalytics(arr)
         }
     }
-]
+} 
 
-var initializeObj = {};
 
-configurations.forEach(function(config){
-    analytics.use(config.package);
-    initializeObj[config.name] = config.settings;
-})
-
-analytics.initialize(initializaObj);
-analytics.page();
